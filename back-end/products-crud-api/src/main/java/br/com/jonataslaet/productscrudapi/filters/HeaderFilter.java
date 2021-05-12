95b31de56060636444da9348185f3b2dee58a111
Jonatas Laet <jonataslaetprogramador@gmail.com>
2021-05-12 17:17:31 -0300

    Criado um filter de header que inicialmente só tem o filter de cors

---
+++ back-end/products-crud-api/src/main/java/br/com/jonataslaet/productscrudapi/filters/HeaderFilter.java
@@ -0,0 +1,26 @@
+package br.com.jonataslaet.productscrudapi.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HeaderFilter implements Filter {
+
+	private String originPermitida = "http://localhost:4200";
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletResponse res = (HttpServletResponse) response;
+		res.setHeader("Access-Control-Allow-Origin", originPermitida);
+		chain.doFilter(request, response);
+	}
+}
