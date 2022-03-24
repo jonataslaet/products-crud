4ed291cb3ea2689889ef1d0506c75eb2b3748492
jonataslaet <jonataslaetprogramador@gmail.com>
2022-03-24 09:09:22 -0300

    Envio de pasta target

---
+++ back-end/products-crud-api/target/classes/data.sql
@@ -0,0 +1,8 @@
+/*
+	Codigo SQL para registro inicial de produtos
+	Author: Jonatas Laet (github.com/jonataslaet)
+*/
+
+INSERT INTO product (name, description, available) VALUES ('Toalha', 'Grande, preta e vermelha', 1);
+INSERT INTO product (name, description, available) VALUES ('Notebook', 'Fino, rápido e potente', 1);
+INSERT INTO product (name, description, available) VALUES ('Carro', 'Pequeno, econômico e bonito', 1);
